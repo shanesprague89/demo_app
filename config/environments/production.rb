49fa61d175ab8fe9414484af69127e050ba44005
Shane Sprague <shanesprague89@gmail.com>
2014-06-28 08:44:12 -0400

    Done I hope

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  group :production do
+      gem 'pg', '0.15.1'
+      gem 'rails_12factor', '0.0.2'
+  end
+  
 end
